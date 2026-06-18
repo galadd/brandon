68c6a2681629aaa2fa5c98d9f1c28c7b79960dc0
galadd <gbolahanf.14@gmail.com>
2026-06-18 05:55:42 +0100

    clean up

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,10 @@
+pub mod convert;
 pub mod error;
 pub mod format;
-
-// #[cfg(feature = "read")]
-pub mod read;
-
-// #[cfg(feature = "write")]
-pub mod write;
-
-// #[cfg(feature = "verify")]
-pub mod verify;
-
-// #[cfg(feature = "convert")]
-pub mod convert;
-
 pub mod fs;
+pub mod read;
+pub mod verify;
+pub mod write;
 
 pub use read::{
     Block, Era1Block, EraBlock, EraBlockReader, EraFormat, EraRandomReader, EraReader,
